63d9af8ead6e4b0d1a30b01a5ca00644ac6ee6e8
Luke Benstead <luke@hughes.(none)>
2008-07-02 12:40:16 +0100

    Removed scenemanager2, going to use a branch instead

--- src/scene/scenemanager2/scene_node.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCENE_NODE_H_INCLUDED
-#define SCENE_NODE_H_INCLUDED
-
-class scene_node_interface {
-	public:
-		virtual void add_child(scene_node_interface* child) = 0;
-};
-
-#endif // SCENE_NODE_H_INCLUDED
