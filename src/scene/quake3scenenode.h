b298dd58325334064c4bc6acbff0556ff6487d6e
Luke Benstead <luke@helium.(none)>
2008-07-06 12:01:16 +0100

    Added a folder for unit tests

--- src/scene/quake3scenenode.h
+++ src/scene/quake3scenenode.h
@@ -7,6 +7,9 @@ class quake3_scene_node :  public scene_node, public map_scene_node_interface {
 
 		void on_register_scene_node();
 
+	private:
+		shared_ptr<base_map> m_map;
+		shared_ptr<map_renderer_interface> m_map_renderer;
 };
 
 #endif // QUAKE3SCENENODE_H_INCLUDED
