0ef32bfe001ded4cb3b56ee6c0aafe534d6d2015
Luke Benstead <kazade@gmail.com>
2008-08-14 13:50:18 +0100

    Added the files to git

---
+++ src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/mat4.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/mat4.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/plane.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/plane.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/quaternion.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/quaternion.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/utility.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/utility.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/vec2.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/vec2.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/vec3.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/vec3.o"
+  "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/src/vec4.c" "/home/luke/Projects/KazEngine3/kazengine/src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/vec4.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ src/kazmath/build-linux/bin/CMakeFiles/Kazmath.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/Kazmath.dir/mat4.o"
+"CMakeFiles/Kazmath.dir/plane.o"
+"CMakeFiles/Kazmath.dir/vec4.o"
+"CMakeFiles/Kazmath.dir/quaternion.o"
+"CMakeFiles/Kazmath.dir/vec2.o"
+"CMakeFiles/Kazmath.dir/vec3.o"
+"CMakeFiles/Kazmath.dir/utility.o"
+"libKazmath.pdb"
+"libKazmath.a"
+"libKazmath.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/Kazmath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
